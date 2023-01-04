6e923b20fe9ba521bcb1618ed52d9cc629436122
sbcshop2 <120171682+sbcshop2@users.noreply.github.com>
2023-01-04 15:05:50 +0530

    Create test.py

---
+++ Examples/test.py
@@ -0,0 +1,13 @@
+import mercury
+import time
+
+time.sleep(2)
+reader = mercury.Reader("tmr:///dev/ttyS0")
+
+reader.set_region("EU3")
+reader.set_read_plan([1], "GEN2")
+
+while 1:  
+    data=reader.read()
+    if(data):
+        print(data)
